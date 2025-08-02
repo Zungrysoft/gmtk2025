f97cc75f774407f507c6fee5cde4b67d11e31aac
ZungrySoft <ZungrySoftEntertainment@gmail.com>
2025-08-02 04:07:44 -0700

    Leave

--- src/guest_hungry.js
+++ src/guest_hungry.js
@@ -13,7 +13,7 @@ export default class GuestHungry extends Guest {
   speedMultiplier = 1.1
   activityOffset = [-64, 0]
   likes_dancing = 10
-  likes_relax = 23
+  likes_relax = 18
   likes_food_pizza = 25
   likes_food_platter = 17
   likes_game = 0
